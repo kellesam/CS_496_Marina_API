e7ae57fe205cf0130f46509f7d7335f1f2c76093
kellesam <kellesam@oregonstate.edu>
2018-01-23 15:08:23 -0600

    route handlers for slip post and get

--- main.py
+++ main.py
@@ -10,8 +10,8 @@ class Boat(ndb.Model):
 
 class Slip(ndb.Model):
 	number = ndb.IntegerProperty(required=True)
-#	current_boat = ndb.IntegerProperty()
-#	arrival_date = ndb.StringProperty()
+	current_boat = ndb.IntegerProperty()
+	arrival_date = ndb.StringProperty()
 
 class MainPage(webapp2.RequestHandler):
     def get(self):
@@ -55,6 +55,33 @@ class BoatHandler(webapp2.RequestHandler):
 			for boat in boats:
 				boat.key.delete()
 
+class SlipHandler(webapp2.RequestHandler):
+	def post(self):
+		parent_key = ndb.Key(Slip, "parent_slip")
+		slip_data = json.loads(self.request.body)
+		new_slip = Slip(number = slip_data['number'],
+						current_boat = None,
+						arrival_date = None,
+						parent = parent_key)
+		new_slip.put()
+		slip_dict = new_slip.to_dict()
+		slip_dict['self'] = '/slip/' + new_slip.key.urlsafe()
+		self.response.write(json.dumps(slip_dict))
+
+	def get(self, id=None):
+		if id:
+			slip = ndb.Key(urlsafe=id).get()
+			if slip:
+				slip_dict = slip.to_dict()
+				slip_dict['self'] = '/slip/' + id
+				self.response.write(json.dumps(slip_dict))
+		else:
+			all_slips = Slip.query().fetch(1000)
+			slips = []
+			for slip in all_slips:
+				slips.append(slip.to_dict())
+			self.response.write(json.dumps(slips))
+
 allowed_methods = webapp2.WSGIApplication.allowed_methods
 new_allowed_methods = allowed_methods.union(('PATCH',))
 webapp2.WSGIApplication.allowed_methods = new_allowed_methods
@@ -62,5 +89,7 @@ webapp2.WSGIApplication.allowed_methods = new_allowed_methods
 app = webapp2.WSGIApplication([
     ('/', MainPage),
     ('/boat', BoatHandler),
-    ('/boat/(.*)', BoatHandler)
+    ('/boat/(.*)', BoatHandler),
+	('/slip', SlipHandler),
+	('/slip/(.*)', SlipHandler)
 ], debug=True)
